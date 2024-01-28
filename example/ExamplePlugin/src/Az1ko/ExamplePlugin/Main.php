e43bedcd660d095eb06a1ef41cffd1e318f2d427
Askhat Taskovich <154591704+TynaevCode@users.noreply.github.com>
2024-01-28 18:49:28 +0600

    Continued development...

---
+++ example/ExamplePlugin/src/Az1ko/ExamplePlugin/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Az1ko\ExamplePlugin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+use Taskovich\DonateExecute\event\NewDonateEvent;
+
+class Main extends PluginBase implements Listener {
+
+	public function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onDonate(NewDonateEvent $event): void {
+		$this->getLogger()->info(
+			"NEW DONATE!\n" .
+			"From: " . $event->getSenderName() . "\n" .
+			"Amount: " . $event->getAmount() . "\n" .
+			"Currency: " . $event->getCurrency() . "\n" .
+			"Message: " . $event->getAllData()["message"] ?? "idk :)"
+		);
+
+		// $event->cancel();
+	}
+
+}
--- src/Taskovich/DonateExecute/utils/Players.php
+++ src/Taskovich/DonateExecute/utils/Players.php
@@ -16,18 +16,22 @@ class Players
 	 * @param Player $player 
 	 * @return void
 	 */
-	public static function addPlayer(Player $player): void
+	public static function checkAndAddPlayer(Player $player): void
 	{
+		if(!self::isAllowedPlayer($player))
+			return;
 		$name = strtolower($player->getName());
-		self::$players[$name] = $players;
+		self::$players[$name] = $player;
 	}
 
 	/**
 	 * @param Player $player 
 	 * @return void
 	 */
-	public static function removePlayer(Player $player): void
+	public static function checkAndRemovePlayer(Player $player): void
 	{
+		if(!self::isAllowedPlayer($player))
+			return;
 		$name = strtolower($player->getName());
 		unset(self::$players[$name]);
 	}
@@ -37,7 +41,19 @@ class Players
 	 */
 	public static function getPlayers(): array
 	{
-		return array_values(self::$players);
+		return self::$players;
+	}
+
+	/**
+	 * @param Player $player 
+	 * @return bool
+	 */
+	public static function isAllowedPlayer(Player $player): bool
+	{
+		$allowed_players = Configs::getConfig()->get("allowed_players");
+		$name = strtolower($player->getName());
+
+		return in_array($name, $allowed_players);
 	}
 
 }
